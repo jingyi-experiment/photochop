5ed0a0237cfe9dd87f5275746c300da081af57f7
jingyi-experiment <140299496+jingyi-experiment@users.noreply.github.com>
2023-07-22 23:32:30 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,56 +1,34 @@
-// App.js
-import React, { useState } from 'react';
-import axios from 'axios';
+function uploadImages() {
+    const imageInput = document.getElementById("imageInput");
+    const uploadedImages = document.getElementById("uploadedImages");
+    uploadedImages.innerHTML = ""; // Clear previous uploaded images
 
-const App = () => {
-  const [selectedImages, setSelectedImages] = useState([]);
-  const [similarGroups, setSimilarGroups] = useState([]);
-
-  const handleImageUpload = async (event) => {
-    const files = event.target.files;
-    if (files.length > 10) {
-      alert('Please select up to 10 images.');
-      return;
+    for (const file of imageInput.files) {
+        // Display Accepted Images
+        if (file.type.startsWith("image/")) {
+            const img = document.createElement("img");
+            img.src = URL.createObjectURL(file);
+            img.width = 200;
+            uploadedImages.appendChild(img);
+        }
     }
+}
 
-    const formData = new FormData();
-    for (const image of files) {
-      formData.append('images', image);
-    }
+function submitImages() {
+    const imageInput = document.getElementById("imageInput");
+    const progressBar = document.getElementById("progressBar");
+    const progressStatus = document.getElementById("progressStatus");
 
-    try {
-      const response = await axios.post('/api/compare', formData, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-      });
-      setSimilarGroups(response.data.similarGroups);
-    } catch (error) {
-      console.error('Error uploading images:', error);
-    }
-  };
+    // Simulate Progress
+    let progress = 0;
+    const interval = setInterval(() => {
+        progress += 10;
+        progressBar.value = progress;
+        progressStatus.textContent = `${progress}%`;
 
-  return (
-    <div>
-      <h1>Image Similarity Checker</h1>
-      <input type="file" multiple onChange={handleImageUpload} />
-      {similarGroups.length > 0 && (
-        <div>
-          <h2>{similarGroups.length} groups of similar pictures have been identified:</h2>
-          {similarGroups.map((group, index) => (
-            <div key={index}>
-              <h3>Group {index + 1}</h3>
-              <ul>
-                {group.map((image, idx) => (
-                  <li key={idx}>
-                    <img src={image} alt={`Similar Image ${idx}`} />
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default App;
+        if (progress >= 100) {
+            clearInterval(interval);
+            progressStatus.textContent = "Processing completed!";
+        }
+    }, 1000);
+}
